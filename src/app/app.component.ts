0f669c6c164353fc5af6dd72a4b3636fd17a0406
Delfia <lovemyself.11.jl@gmail.com>
2020-09-23 00:26:39 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  viewMode = '';
+
+  form = new FormGroup({
+    'associateName': new FormControl('', [
+      Validators.required, 
+      Validators.minLength(5), 
+      Validators.maxLength(30), 
+      Validators.pattern("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")
+    ]),
+    'associateId': new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[0-9]{6}$")
+    ]),
+    'projectId': new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-zA-Z0-9]{12}$")
+    ]),
+    'offshore': new FormControl('', Validators.required),
+    'onshore': new FormControl('', Validators.required),
+    'comments': new FormControl('', Validators.required)
+  });
+
+  get associateName(){
+    return this.form.get('associateName');
+  }
+
+  get associateId(){
+    return this.form.get('associateId');
+  }
+
+  get projectId(){
+    return this.form.get('projectId');
+  }
+
+  get offshore(){
+    return this.form.get('offshore');
+  }
+
+  get onshore(){
+    return this.form.get('onshore');
+  }
+
+  get comments(){
+    return this.form.get('comments');
+  }
+
+  submit(form){
+    console.log("Successfully Submitted: ", form);
+    
+  }
 }
